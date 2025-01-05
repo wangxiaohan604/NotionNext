60b74e6ee8acad1b4ceead2a8e2ef55a33c40fcf
wangxiaohan604 <wyfwzx621@gmail.com>
2025-01-05 13:57:42 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -1,23 +1,21 @@
-const config = {
-    HEO_HOME_BANNER_ENABLE: true, // 是否启用首页横幅
-    "时间": '2025-01-01',          // 建站日期，用于计算网站运行的第几天
-    "注意事项": [                  // 首页顶部通知条滚动内容，如不需要可以留空 []
-        "欢迎访问我们的网站！",
-        "新年快乐！"
-    ]
-};
+const CONFIG = {
+  HEO_HOME_POST_TWO_COLS: true, // 首页博客两列显示，若为false则只显示一列
+  HEO_LOADING_COVER: true, // 页面加载的遮罩动画
 
+  HEO_HOME_BANNER_ENABLE: true,
+
+  HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
-   注意事项 : [
-     {  标题 : '欢迎来到拾光分享站' , 乌拉圭网址 : 'https://qshare.cc'  } ,
-       {    标题 : '探索更多实用资源' , 乌拉圭网址 : 'https://qshare.cc'    }
-     ] ,
+  HEO_NOTICE_BAR: [
+    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
+    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+  ],
 
   // 英雄区左右侧组件颠倒位置
-     相反的 : 假的 ,
+  HEO_HERO_REVERSE: false,
   // 博客主体区左右侧组件颠倒位置
-     HEO_HERO_BODY_REVERSE : 假的 ,
+  HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
      高_英雄_钛_1 : '分享实用工具资源' ,
