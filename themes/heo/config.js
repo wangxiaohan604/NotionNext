5d7a6eda2be0f48657f624a129a341fc119f29f4
wangxiaohan604 <wyfwzx621@gmail.com>
2025-01-06 15:19:06 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -20,7 +20,7 @@ const CONFIG = {
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '分享实用资源',
   HEO_HERO_TITLE_2: '工具 | 课程 | 副业技巧',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
+  HEO_HERO_TITLE_3: 'qshare.cc',
   HEO_HERO_TITLE_4: '动漫迷的天堂',
   HEO_HERO_TITLE_5: '带你畅游二次元世界',
   HEO_HERO_TITLE_LINK: 'https://qshare.cc/resources/anime-resources-collection-guide',
@@ -33,7 +33,7 @@ const CONFIG = {
   HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
-  HEO_HERO_RECOMMEND_POST_TAG: '推荐',
+  HEO_HERO_RECOMMEND_POST_TAG: 'AI',
   HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
   //   HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片
 
